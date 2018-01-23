57bdbd5236589fc5673600390b2a73e2f4495a31
bram.sierhuis <bram@bramsierhuis.nl>
2018-01-23 22:40:55 +0100

    Removed commented code

--- app/src/main/java/bram/bramsierhuis/nl/workinghours/MainActivity.java
+++ app/src/main/java/bram/bramsierhuis/nl/workinghours/MainActivity.java
@@ -8,14 +8,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-/*
-    @BindView(R.id.usernameEditText) EditText usernameEditText;
-    @BindView(R.id.passEditText) EditText passEditText;
-    @BindView(R.id.confirmPassEditText) EditText confirmPassEditText;
 
-    private Call<String> registerApiCall;
-    private Call<String> loginApiCall;
-*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,75 +27,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
-/*
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (registerApiCall != null) {
-            registerApiCall.cancel();
-        }
-
-        if(loginApiCall != null){
-            loginApiCall.cancel();
-        }
-    }
-
-    @OnClick(R.id.registerBtn)
-    public void onRegister() {
-        registerApiCall = ((App)getApplicationContext()).getWorkingHoursApi().register(
-                usernameEditText.getText().toString(),
-                passEditText.getText().toString(),
-                confirmPassEditText.getText().toString()
-        );
-
-        registerApiCall.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                if (response.isSuccessful()) {
-                    //TODO Handle register
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("test")
-                            .setMessage(response.body())
-                            .create()
-                            .show();
-                } else {
-                    //TODO Server responded with an error
-                }
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                //TODO A network error occurred
-            }
-        });
-    }
-
-    @OnClick(R.id.loginBtn)
-    public void onLogin(){
-        loginApiCall = ((App)getApplicationContext()).getWorkingHoursApi().login(
-                usernameEditText.getText().toString(),
-                passEditText.getText().toString()
-        );
-
-        loginApiCall.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                if (response.isSuccessful()) {
-                    //TODO Handle login
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle(R.string.alert)
-                            .setMessage(response.body())
-                            .create()
-                            .show();
-                } else {
-                    //TODO Server responded with an error
-                }
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                //TODO A network error occurred
-            }
-        });
-    }*/
 }
